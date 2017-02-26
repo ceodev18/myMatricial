7bbbe6afbd28661a3683fef4a19db126e49c6ce5
antaresr <diagonal_zero@hotmail.com>
2017-02-26 14:14:33 -0500

    beggining matricial answer

---
+++ src/helpers/matricialVariation/MatricialTester.java
@@ -0,0 +1,23 @@
+package helpers.matricialVariation;
+
+import algorithm.base.LSystemRouteAlgorithm;
+import algorithm.base.LotizationAlgorithm;
+import interfaces.Constants;
+import models.LandMap;
+import models.LandPoint;
+
+public class MatricialTester {
+	public static void main(String[] args) {
+		LandMap landMap = new LandMap(40, 40);
+		LandPoint landPoint = new LandPoint(20, 0, false, false, true, false, false);
+		LSystemRouteAlgorithm.landMap = landMap;
+		LSystemRouteAlgorithm.createRoute(landPoint.getId(), Constants.NORTH, Constants.AVENUE_BRANCH, 120, 0);
+		//LSystemRouteAlgorithm.landMap.printMap();
+		
+		LotizationAlgorithm.landMap = LSystemRouteAlgorithm.landMap;
+		int lotSize = 240;
+		LotizationAlgorithm.generateLots(lotSize);
+		LotizationAlgorithm.landMap.printMap();
+		
+	}
+}
