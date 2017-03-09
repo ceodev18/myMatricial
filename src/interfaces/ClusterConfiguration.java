b63dd7daed7633ec269319fe3e2683160769e920
antaresr <diagonal_zero@hotmail.com>
2017-03-09 14:21:09 -0500

    d

--- src/interfaces/ClusterConfiguration.java
+++ src/interfaces/ClusterConfiguration.java
@@ -11,7 +11,8 @@ public interface ClusterConfiguration {
 	public static int HOUSE_SIDE_MINIMUN_SIZE = 8;
 	public static int HOUSE_SIDE_MAXIMUN_SIZE = 15;
 	public static int HOUSE_DEPTH_MINIMUN_SIZE = 12; //8*12=96	
-
+	public static int HOUSE_DEPTH_MAXIMUN_SIZE = 12; //8*12=96	
+	
 	public static int ARTERIAL_BRANCH = 0;
 	public static int COLLECTOR_BRANCH = 1;
 	public static int LOCAL_BRANCH = 2;
